00e60d1b62a7854efb4ff406150e2a328648de54
anjd <ite.anjd@outlook.com>
2018-05-18 00:35:26 +0300

    initial

--- message/vqa_message.py
+++ message/vqa_message.py
@@ -3,5 +3,6 @@ from .message import Message
 
 class VqaMessage(Message):
     def __init__(self, image, question):
-        super().__init__(image)
+        super().__init__()
+        self.image = image
         self.question = question
