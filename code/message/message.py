bc8d9764be80c35a1948c6a145f2d62e15b5ca42
anjd <ite.anjd@outlook.com>
2018-05-31 14:03:25 +0300

    json

--- code/message/message.py
+++ code/message/message.py
@@ -1,4 +1,5 @@
 class Message:
     def __init__(self):
-        self._type = self.__class__.__name__.encode()
+        self._type = "Hell".encode()
+        # self._type = self.__class__.__name__.encode()
         # pass
