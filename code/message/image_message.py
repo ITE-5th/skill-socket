98a603de13c2594587c434537fb9c31c31f8782a
anjd <ite.anjd@outlook.com>
2018-05-31 14:20:23 +0300

    json

--- code/message/image_message.py
+++ code/message/image_message.py
@@ -5,5 +5,6 @@ class ImageMessage(Message):
     def __init__(self, image):
         super().__init__()
         self.image = image
-        self.tttt = self.__class__.__name__.encode()
+        # self.tttt = self.__class__.__name__.encode()
+        self.tttt = 1234
 
