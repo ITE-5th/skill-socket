14574a6fbb5cbce21ed737842c71178460d196ad
anjd <ite.anjd@outlook.com>
2018-05-31 13:38:34 +0300

    json

--- __init__.py
+++ __init__.py
@@ -60,7 +60,7 @@ class SocketSkill(MycroftSkill):
         try:
             image = self.take_image()
             LOG.info(type(image))
-            msg = ImageToTextMessage("123")
+            msg = ImageToTextMessage(image.tolist())
             ConnectionHelper.send_json(self.socket, msg)
             result = ConnectionHelper.receive_json(self.socket)
             LOG.info(result)
